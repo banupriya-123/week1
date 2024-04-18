7504dfc9d92e3b2f5f5fbf26cce9954a9aaba160
banupriya-123 <167317879+banupriya-123@users.noreply.github.com>
2024-04-18 09:35:27 +0530

    Add files via upload

---
+++ week1/day1/FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+	
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isvisible=true;
+		int releaseyear=1998;
+		char browserlogo='f';
+		
+		System.out.println(browserVersion+"\n"+browserName+"\n"+isvisible+"\n"+releaseyear+"\n"+browserlogo);
+				
+
+
+	}
+
+}
---
+++ week1/day2/FindOddNumber.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class FindOddNumber {
+	public static void main(String[] args) {
+		int number=10;
+		for(int i=0;i<=number;i++) {
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
